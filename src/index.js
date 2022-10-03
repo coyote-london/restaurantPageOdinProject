ab8165f7043e98e4d146e2b7be14a99cfc1f05ad
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-10-03 16:00:42 -0500

    Add webpage/image directories and add aboutPage.js, menuPage.js, and contactPage.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import css from './style.css';
-import imgBreadFrame from './BreadFrameMitiUnsplash.jpg'
+import imgBreadFrame from './images/BreadFrameMitiUnsplash.jpg'
+import { test } from './webpages/aboutPage.js'
 
-console.log('Works');
---
+++ src/webpages/aboutPage.js
@@ -0,0 +1,5 @@
+const test = (() => {
+    console.log('Export works')
+})();
+
+export { test }
