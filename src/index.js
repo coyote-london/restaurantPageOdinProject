7a81a6d1d59cf37e0f99156cc747bc080c009d64
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-10-05 06:12:08 -0500

    Change font to romance logic and make final styling touchs

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import imgBreadFrame from './images/BreadFrameMitiUnsplash.jpg'
 import { renderAboutPage } from './webpages/aboutPage.js';
 import { renderContactPage } from './webpages/contactPage.js';
 import { renderMenuPage } from './webpages/menuPage';
+import romanceLogicFont from './fonts/romance-logic-font/RomanceLogic.ttf';
 renderAboutPage();
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
                 test: /\.(png|jpg|svg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
+            
+            {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            },
         ],
     },
     
